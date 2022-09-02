b3870b2e67cde7d5089f3bdf15d03aad2bf26e19
Théo DURR <git@theodurr.fr>
2022-09-02 22:08:34 +0200

    ✨ database is now set up for all environments
    See : https://github.com/ae-utbm/api/wiki/Databases

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './v1/prisma/prisma.module';
 import { ModuleV1 } from './v1/v1.module';
 
 @Module({
-  imports: [ModuleV1, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      // Make the config module alvailable everywhere
+      isGlobal: true,
+    }),
+    ModuleV1,
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
