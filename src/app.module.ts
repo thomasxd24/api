4d7ae32a07bde2e3cfc996f8f46a75f39383069b
Théo DURR <git@theodurr.fr>
2022-10-01 19:46:01 +0200

    Added sentry error interceptions (#20)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { HttpException, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { SentryInterceptor, SentryModule } from '@ntegral/nestjs-sentry';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './v1/prisma/prisma.module';
@@ -13,8 +15,26 @@ import { ModuleV1 } from './v1/v1.module';
       isGlobal: true,
       envFilePath: ['.env', '.env.example'],
     }),
+    SentryModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        dsn: config.getOrThrow<string>('SENTRY_DSN'),
+        debug: false,
+        environment: config.getOrThrow<string>('NODE_ENV'),
+        logLevels: ['error']
+      }),
+      inject: [ConfigService]
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_INTERCEPTOR,
+    useFactory: () => new SentryInterceptor({
+      filters: [{
+        type: HttpException,
+        filter: (exception: HttpException) => 500 > exception.getStatus()
+      }]
+    })
+  }],
 })
 export class AppModule {}
